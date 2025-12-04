e4b0098af490e50bfeed8a887c4ab7810ef9a7c3
Qile Jiang <qile.jiang@outlook.com>
2025-12-03 21:50:40 -0500

    Use conda opencv with LD_LIBRARY_PATH

--- run_shared_memory.sh
+++ run_shared_memory.sh
@@ -7,12 +7,20 @@
 #SBATCH --ntasks=1
 #SBATCH --partition=mi3001x
 
-# Load modules 
+# Load rocm
 module load rocm
-module load opencv
 
-# Compile
-hipcc -O3 -o impl1_shared_memory impl1_shared_memory.cpp `pkg-config --cflags --libs opencv4`
+# Activate conda environment with OpenCV
+source $HOME/miniconda3/bin/activate horn
+
+# Compile with conda's OpenCV
+hipcc -O3 -o impl1_shared_memory impl1_shared_memory.cpp \
+    -I$CONDA_PREFIX/include/opencv4 \
+    -L$CONDA_PREFIX/lib \
+    -lopencv_core -lopencv_imgproc -lopencv_videoio -lopencv_imgcodecs
+
+# Set library path for runtime
+export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
 
 # Run
 ./impl1_shared_memory
